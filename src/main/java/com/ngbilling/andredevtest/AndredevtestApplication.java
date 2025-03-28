5d54f3e4a539ac070e054b48a29650ff157c09e1
André Luiz Nunes Goulart <andreluizalemao@gmail.com>
2025-03-28 13:27:31 -0300

    Initial commit

---
+++ src/main/java/com/ngbilling/andredevtest/AndredevtestApplication.java
@@ -0,0 +1,13 @@
+package com.ngbilling.andredevtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndredevtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndredevtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngbilling/andredevtest/AndredevtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngbilling.andredevtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndredevtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
